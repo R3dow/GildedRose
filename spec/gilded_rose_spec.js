86365c9b35ab83a7d811eaf33ad0250a1a52f18b
Muhittin <muhittinsungur40@gmail.com>
2026-06-01 22:53:57 +0200

    added backstage pass increase with 10 days left

--- spec/gilded_rose_spec.js
+++ spec/gilded_rose_spec.js
@@ -61,4 +61,13 @@ describe("Gilded Rose", function() {
     expect(items[0].quality).toEqual(21);
   });
 
+  it("backstage passes increase quality by 2 when sell_in is 10 days or less", function() {
+    items = [ new Item("Backstage passes to a TAFKAL80ETC concert", 10, 20) ];
+
+    update_quality();
+
+    expect(items[0].sell_in).toEqual(9);
+    expect(items[0].quality).toEqual(22);
+  });
+
 });
