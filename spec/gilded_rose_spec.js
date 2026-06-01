207bee261cbdc2dd8ac9dd7f29e3e036a36fb153
Muhittin <muhittinsungur40@gmail.com>
2026-06-01 22:57:49 +0200

    added iteam degredation for conujred item

--- spec/gilded_rose_spec.js
+++ spec/gilded_rose_spec.js
@@ -88,4 +88,13 @@ describe("Gilded Rose", function() {
     expect(items[0].quality).toEqual(0);
   });
 
+  it("conjured items decrease quality twice as fast as normal items", function() {
+    items = [ new Item("Conjured Mana Cake", 10, 20) ];
+
+    update_quality();
+
+    expect(items[0].sell_in).toEqual(9);
+    expect(items[0].quality).toEqual(18);
+  });
+
 });
