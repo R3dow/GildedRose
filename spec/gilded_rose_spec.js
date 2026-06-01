0610b066e3969af887f9dec959e22cbcadee78b7
Muhittin <muhittinsungur40@gmail.com>
2026-06-01 22:41:12 +0200

    added the testing for normal item quality degredatoin

--- spec/gilded_rose_spec.js
+++ spec/gilded_rose_spec.js
@@ -1,9 +1,12 @@
 describe("Gilded Rose", function() {
 
-  it("should foo", function() {
-    items = [ new Item("foo", 0, 0) ];
+  it("normal item decreases quality and sell_in by 1", function() {
+    items = [ new Item("+5 Dexterity Vest", 10, 20) ];
+
     update_quality();
-    expect(items[0].name).toEqual("fixme");
+
+    expect(items[0].sell_in).toEqual(9);
+    expect(items[0].quality).toEqual(19);
   });
 
 });
